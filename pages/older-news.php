9d19215ba502cf5632c39f3235f921082a060e82
hajdam <hajdam@users.sf.net>
2018-04-15 23:34:17 +0200

    Split into older news

---
+++ pages/older-news.php
@@ -0,0 +1,112 @@
+<div id="content">
+<a name="news"><h2>Older News</h2></a>
+<li><strong>2017-10-10:</strong> DeltaHex NetBeans Plugin 0.1.7 Released<p>
+<ul><li>Values panel made editable</li>
+<li>Byte order support for floating point numbers in values panel</li>
+<li>Added thread for values panel updating</li>
+<li>Fixed files closing on window closing</li>
+<li>Fixed clipboard charset translation</li>
+</ul></p></li>
+
+<li><strong>2017-04-01:</strong> DeltaHex IntelliJ Plugin 0.1.4 Released<p>
+<ul><li>Fixed behavior for high precision wheel scrolling</li>
+<li>Fixed loading after custom font saved</li>
+<li>Fixed opening of read-only files</li>
+</ul></p></li>
+
+<li><strong>2017-04-01:</strong> DeltaHex NetBeans Plugin 0.1.6 Released<p>
+<ul><li>Fixed behavior for high precision wheel scrolling</li>
+<li>Fixed opening of read-only files</li>
+</ul></p></li>
+
+<li><strong>2017-03-22:</strong> DeltaHex IntelliJ Plugin 0.1.3 Released<p>
+<ul><li>Added options dialog</li>
+<li>Added values side panel</li>
+<li>Added "File/Open File as Hex" action</li>
+<li>Code colorization by byte value</li>
+<li>Some minor fixes</li>
+</ul></p></li>
+
+<li><strong>2017-03-22:</strong> DeltaHex NetBeans Plugin 0.1.5 Released<p>
+<ul><li>Added options panel / dialog</li>
+<li>Added values side panel</li>
+<li>Added "File/Open File as Hex" action</li>
+<li>Code colorization by byte value</li>
+<li>Some minor fixes</li>
+</ul></p></li>
+
+<li><strong>2017-02-04:</strong> DeltaHex IntelliJ Plugin 0.1.2 Released<p>
+<ul><li>Fixed issue in Android Studio 2.2.3 (issue #1)</li>
+<li>Fixed issue with saving in delta mode</li>
+</ul></p></li>
+
+<li><strong>2017-01-11:</strong> DeltaHex IntelliJ Plugin 0.1.1 Released<p>
+<ul><li>Fixed issue on Android Studio</li>
+<li>Added status panel</li>
+<li>Added go-to position dialog</li>
+<li>Added crude search panel</li>
+<li>Adding charset now support basic filtering</li>
+<li>Enabled delta mode, switchable to memory mode</li>
+<li>Most settings are now persistent</li>
+</ul></p></li>
+
+<li><strong>2017-01-08:</strong> DeltaHex NetBeans Plugin 0.1.4 Released<p>
+<ul><li>Fixed charset for clipboard copy/paste</li>
+<li>Added status panel</li>
+<li>Added go-to position dialog</li>
+<li>Added crude search panel</li>
+<li>Adding charset now support basic filtering</li>
+<li>Enabled delta mode, switchable to memory mode</li>
+<li>Most settings are now persistent</li>
+</ul></p></li>
+
+<li><strong>2017-01-07:</strong> DeltaHex Editor 0.1.2 Released<p>
+New version of Delta Hexadecimal Editor released.
+<ul><li>Fixed charset for clipboard copy/paste</li>
+<li>Modified status panel to show relative size</li>
+<li>Adding charset now support basic filtering</li>
+<li>Enabled delta mode, switchable to memory mode</li>
+<li>Options panels now have Save button</li>
+</ul></p></li>
+
+<li><strong>2016-12-13:</strong> DeltaHex IntelliJ Plugin 0.1.0 Released<p></p></li>
+
+<li><strong>2016-12-05:</strong> DeltaHex Editor 0.1.1 Released<p>
+New version of Delta Hexadecimal Editor released.
+<ul><li>Added main window position saving</li>
+<li>Fixed charset for clipboard copy/paste</li>
+<li>Fixed issue with Preferences on Windows Vista and newer</li>
+<li>Fixed issue with window headers for dark themes</li>
+<li>Added windows executable and NSIS installer</li>
+<li>Full cursor shape and cursor blinking</li>
+<li>Support for horizontal position offset</li>
+<li>Added copy as code and paste from code</li>
+<li>Fixed clipboard related IllegalStateException issue</li>
+</ul></p></li>
+
+<li><strong>2016-10-05:</strong> DeltaHex NetBeans Plugin 0.1.3 Released<p>
+<ul><li>Full cursor shape and cursor blinking</li>
+<li>Support for horizontal position offset</li>
+<li>Fixed NPE on Windows 7 (Bug 267973)</li>
+</ul></p></li>
+
+<li><strong>2016-07-23:</strong> DeltaHex NetBeans Plugin 0.1.2 Released<p>
+<ul><li>Added copy as code and paste from code</li>
+<li>Fixed opening of .shadow files</li>
+<li>Fixed clipboard related IllegalStateException issue</li>
+<li>Improved plugin description2016-07-23
+</ul></p></li>
+
+<li><strong>2016-07-20:</strong> DeltaHex Editor 0.1.0 Released<p></p></li>
+
+<li><strong>2016-06-25:</strong> DeltaHex NetBeans Plugin 0.1.1 Released<p>
+<ul><li>Added binary, decimal and octal mode</li>
+<li>Partially fixed "Open as Hex" menu item position</li>
+<li>Slightly improved rendering performance</li>
+</ul></p></li>
+
+<li><strong>2016-06-08:</strong> DeltaHex NetBeans Plugin 0.1.0 Released<p></p></li>
+</ul>
+</div>
+</body>
+</html>
