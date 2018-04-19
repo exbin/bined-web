2df30d6e8ae47df9116daf84900680cfb0d26fab
hajdam <hajdam@users.sf.net>
2018-04-19 21:49:46 +0200

    Adding more subpages stubs - 2

---
+++ pages/screenshots.php
@@ -0,0 +1,8 @@
+<div id="content">
+<a name="screenshots"><h2>Screenshots</h2></a>
+<h2>Editor</h2>
+<p><img src="images/editor_screenshot.png" alt="Editor screenshot" title="Editor screenshot"/></p>
+
+</div>
+</body>
+</html>
