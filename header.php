e3f38d95d1a2efe0dff61c7e8e52ad7ed4ae251d
hajdam <hajdam@users.sf.net>
2020-01-14 20:53:19 +0100

    Reworking menu

--- header.php
+++ header.php
@@ -22,41 +22,42 @@ if (!empty($prefix)) {
 </div>
 <div id="divider"></div>
 
-<div id="sidebar">
-<h4>&nbsp;&nbsp;&nbsp;Information</h4>
-<ul>
-  <li><a href="<?php echo $rootPrefix; ?>?news">News</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?about">About</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?screenshots">Screenshots</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?downloads">Downloads</a></li>
+<ul id="navmenu">
+  <li><div>Information</div>
+    <ul class="submenu">
+    <li><a href="<?php echo $rootPrefix; ?>?news">News</a></li>
+    <li><a href="<?php echo $rootPrefix; ?>?about">About</a></li>
+    <li><a href="<?php echo $rootPrefix; ?>?screenshots">Screenshots</a></li>
+    <li><a href="<?php echo $rootPrefix; ?>?downloads">Downloads</a></li>
+    </ul>
+  </li>
+  <li><div>Variants</div>
+    <ul class="submenu">
+      <li><a href="<?php echo $parentPrefix; ?>editor">Editor - Java</a><?php echo $submenu_editor; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>android">Editor - Android</a><?php echo $submenu_android; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>library">Component Libraries</a><?php echo $submenu_library; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>basic-editor">Basic Editor</a><?php echo $submenu_basiceditor; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>netbeans-plugin">NetBeans Plugin</a><?php echo $submenu_netbeansplugin; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>intellij-plugin">IntelliJ Plugin</a><?php echo $submenu_intellijplugin; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>eclipse-plugin">Eclipse Plugin</a><?php echo $submenu_eclipseplugin; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>jdeveloper-extension">JDeveloper Extension</a><?php echo $submenu_jdeveloperextension; ?></li>
+      <li><a href="<?php echo $parentPrefix; ?>bluej-extension">BlueJ Extension</a><?php echo $submenu_bluejextension; ?></li>
+    </ul>
+  </li>
+  <li><div>Development</div>
+    <ul class="submenu">
+      <li><a href="<?php echo $rootPrefix; ?>?participate">Participate</a></li>
+      <li><a href="<?php echo $rootPrefix; ?>?features">Features</a></li>
+      <li><a href="<?php echo $rootPrefix; ?>?concepts">Concepts</a></li>
+      <li><a href="<?php echo $rootPrefix; ?>?source-codes">Source Codes</a></li>
+      <li><a href="<?php echo $rootPrefix; ?>?donate">Donate</a></li>
+    </ul>
+  </li>
+  <li><div>Social</div>
+    <ul class="submenu">
+      <li><a class="urlextern" href="https://sourceforge.net/projects/bined/">SourceForge</a></li>
+      <li><a class="urlextern" href="https://www.openhub.net/p/bined/">OpenHub</a></li>
+      <li><a href="<?php echo $rootPrefix; ?>?similar-projects">Similar Projects</a></li>
+    </ul>
+  </li>
 </ul>
-
-<h4>&nbsp;&nbsp;&nbsp;Variants</h4>
-<ul>
-  <li><a href="<?php echo $parentPrefix; ?>editor">Editor - Java</a><?php echo $submenu_editor; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>android">Editor - Android</a><?php echo $submenu_android; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>library">Component Libraries</a><?php echo $submenu_library; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>basic-editor">Basic Editor</a><?php echo $submenu_basiceditor; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>netbeans-plugin">NetBeans Plugin</a><?php echo $submenu_netbeansplugin; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>intellij-plugin">IntelliJ Plugin</a><?php echo $submenu_intellijplugin; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>eclipse-plugin">Eclipse Plugin</a><?php echo $submenu_eclipseplugin; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>jdeveloper-extension">JDeveloper Extension</a><?php echo $submenu_jdeveloperextension; ?></li>
-  <li><a href="<?php echo $parentPrefix; ?>bluej-extension">BlueJ Extension</a><?php echo $submenu_bluejextension; ?></li>
-</ul>
-
-<h4>&nbsp;&nbsp;&nbsp;Development</h4>
-<ul>
-  <li><a href="<?php echo $rootPrefix; ?>?participate">Participate</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?features">Features</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?concepts">Concepts</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?source-codes">Source Codes</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?donate">Donate</a></li>
-</ul>
-
-<h4>&nbsp;&nbsp;&nbsp;Social</h4>
-<ul>
-  <li><a class="urlextern" href="https://sourceforge.net/projects/bined/">SourceForge</a></li>
-  <li><a class="urlextern" href="https://www.openhub.net/p/bined/">OpenHub</a></li>
-  <li><a href="<?php echo $rootPrefix; ?>?similar-projects">Similar Projects</a></li>
-</ul>
-</div>
