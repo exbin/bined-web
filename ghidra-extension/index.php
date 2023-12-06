a6da68e4a9683acf9398fb9102eb7f7c95484c92
hajdam <hajdam@users.sf.net>
2023-12-06 22:52:34 +0100

    Lazy day

---
+++ ghidra-extension/index.php
@@ -0,0 +1,35 @@
+<?php global $prefix, $submenu_editor;
+$prefix = '..';
+$submenu_ghidraextension = '<ul><li><a class="urldecor" style="background-image: url(\''.@$parentPrefix.'../images/menu/help-contents-2.png\');" href="?manual"><del>Manual</del></a></li></ul>';
+$childIndex = 'ghidra-extension';
+include('../header.php');
+$query = getenv('QUERY_STRING');
+if (empty($query)) {
+  $include = 'pages/main.php';
+} else {
+  $paramPos = strpos($query, '&');
+  if ($paramPos !== false) {
+    if ($paramPos > 0 && ("=" == $query[$paramPos - 1])) {
+  	   $query = substr($query, 0, $paramPos - 1);
+    } else {
+  	   $query = substr($query, 0, $paramPos);
+  	}
+  } else {
+  	$length = strlen($query);
+  	if ($length > 0 && ("=" == $query[$length - 1])) {
+  		$query = substr($query, 0, $length - 1);
+  	}
+  }
+  $target = 'pages/'.$query.'.php';
+  if (!(preg_match("/^[a-z\_\-]+$/", $query) === false) && file_exists($target)) {
+    $include = $target;
+  } else {
+  	http_response_code(404);
+    $include = 'pages/not-found.php';
+  }
+}
+
+include $include;
+
+include '../refer.php';
+?>
---
+++ ghidra-extension/pages/main.php
@@ -0,0 +1,36 @@
+<?php include '../download/download-list.php'; $downloads = getDownloadList('ghidra-extension');
+function echoDownload($arr) {
+  foreach ($arr as $key => $value) {
+    if ($key > 0) echo "<br/>\n";
+    echo @$value['prefix'].'<a href="../download/?'.$value['file'].'" title="Download '.$value['name'].'"><img src="../images/filetypes/'.$value['icon'].'.png" alt="['. strtoupper($value['icon']).']" width="16" height="16"/> '.$value['ver'].'</a> ['.$value['date'].']';
+  }
+} ?><div id="content">
+<h1 id="extension">Extension: Ghidra Extension</h1>
+
+<p>Binary/hex editor extension for <a class="urlextern" href="https://ghidra-sre.org/">Ghidra SRE</a> written in Java.</p>
+
+<p><img src="images/ghidra-screenshot.png" alt="Extension screenshot" title="Extension screenshot" width="800"/></p>
+
+<h2 id="downloads">Downloads</h2>
+<table class="downloads">
+<tr><th>Release&nbsp;</th><th>Stable&nbsp;</th><th class="dev">Development&nbsp;</th></tr>
+<tr><td>Ghidra Extension</td>
+  <td><?php echoDownload($downloads['ghidra-extension']['stb']); ?></td>
+  <td><?php echoDownload($downloads['ghidra-extension']['dev']); ?></td>
+</tr>
+</table>
+
+<h2 id="installation">Installation</h2>
+<p>Unzip jar file from downloaded zip file into Ghidra/Extensions directory of your Ghidra installation.</p>
+
+<p>Show Bined Window.</p>
+
+<h2 id="source_codes">Source Codes</h2>
+<table class="downloads">
+<tr><th>GitHub Repository&nbsp;</th></tr>
+<tr><td><a href="https://github.com/exbin/ghidra-extension" title="GitHub repository"><img src="../images/ref/github.png" alt="[GitHub]"/> exbin/ghidra-extension</a></td></tr>
+</table>
+
+</div>
+</body>
+</html>
